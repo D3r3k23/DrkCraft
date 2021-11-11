a14ac6bcee747f2a71527f40b45513868e6e2bb6
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-11-10 16:16:46 -0800

    Transform

--- src/Graphics/Transform.cpp
+++ src/Graphics/Transform.cpp
@@ -2,5 +2,43 @@
 
 namespace DrkCraft
 {
+    Transform::Transform(const glm::mat4& matrix)
+      : m_matrix(matrix)
+    { }
 
+    Transform Transform::Translation(double x, double y, double z)
+    {
+        return Transform(glm::mat4(1.0f));
+    }
+
+    Transform Transform::Scale(double s)
+    {
+        return Transform(glm::mat4(1.0f));
+    }
+
+    Transform Transform::Rotation(double theta)
+    {
+        return Transform(glm::mat4(1.0f));
+    }
+
+    Transform Transform::Normal(void)
+    {
+        return Transform(glm::mat4(1.0f));
+    }
+
+    Transform::operator glm::mat4(void) const
+    {
+        return m_matrix;
+    }
+
+    Transform Transform::operator*(const Transform& other) const
+    {
+        return m_matrix * other.m_matrix;
+    }
+
+    Transform& Transform::operator*=(const Transform& other)
+    {
+        *this = *this * other;
+        return *this;
+    }
 }
--- src/Graphics/Transform.hpp
+++ src/Graphics/Transform.hpp
@@ -8,15 +8,25 @@ namespace DrkCraft
     class Transform
     {
     private:
-        glm::mat4 matrix;
+        glm::mat4 m_matrix;
 
     public:
-        Transform(glm::mat4 matrix) : matrix(matrix) { }
+        Transform(const glm::mat4& matrix);
+
+        Transform(const Transform&) = default;
+        Transform(Transform&&) = default;
+        Transform& operator=(const Transform&) = default;
+        Transform& operator=(Transform&&) = default;
 
         static Transform Translation(double x, double y, double z);
         static Transform Scale(double s);
         static Transform Rotation(double theta);
         static Transform Normal(void);
+
+        operator glm::mat4(void) const;
+
+        Transform operator*(const Transform& other) const;
+        Transform& operator*=(const Transform& other);
     };
 }
 
