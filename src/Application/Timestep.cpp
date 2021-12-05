e93b93cc75fdf02cdb82fab22cf6ec2c586b7dbc
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-12-04 17:00:08 -0800

    Max timestep

--- src/Application/Timestep.cpp
+++ src/Application/Timestep.cpp
@@ -1,11 +1,16 @@
 #include "Timestep.hpp"
 
+#include <algorithm>
+
 namespace DrkCraft
 {
+    // If framerate drops below 5 FPS, game will slow down
+    const double MAX_TIMESTEP = 1.0 / 5;
+
     Timer Timestep::s_frameTimer(0.0);
 
     Timestep::Timestep(void)
-      : m_elapsed(s_frameTimer.elapsed_seconds())
+      : m_elapsed(std::min(s_frameTimer.elapsed_seconds(), MAX_TIMESTEP))
     {
         s_frameTimer.reset();
     }
