e04fee35d5f9381022d5344a5707846cfd1af7e4
Derek Henderson <derekhenderson818@gmail.com>
2022-04-21 07:11:42 -0700

    py main

--- scripts/drkcraft-py/drkcraft/__main__.py
+++ scripts/drkcraft-py/drkcraft/__main__.py
@@ -18,17 +18,18 @@ CMD = {
 def main(argv: list[str]=sys.argv) -> Optional[int]:
     sys.argv[0] = 'drkcraft'
 
+    help: bool = any(h in argv for h in { '-h', '--help' })
+
     parser = argparse.ArgumentParser(description='Python utilities for DrkCraft', add_help=False)
     parser.add_argument('-h', '--help', action='store_true', help='show this help message and exit')
     parser.add_argument('-V', '--version', action='version', version=drkcraft.config.get_version())
     parser.add_argument('cmd', type=str, metavar='cmd', help='[build|run|clean|package]',
-        choices=(list(CMD.keys()) + ([''] if help_in(argv) else [])),
-        nargs=('?' if help_in(argv) else None), default='')
+        choices=(list(CMD.keys()) + ([''] if help else [])), nargs=('?' if help else '+'), default='')
     args = parser.parse_args(argv[1:])
 
     cmd_name = args.cmd
 
-    if args.help:
+    if help:
         if args.cmd == '':
             parser.print_help()
             return 0
@@ -38,9 +39,6 @@ def main(argv: list[str]=sys.argv) -> Optional[int]:
         cmd_args = sys.argv[2:]
 
     return CMD[cmd_name](cmd_args)
-
-def help_in(args: Sequence[str]) -> bool:
-    return any(h in args for h in { '-h', '--help' })
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
