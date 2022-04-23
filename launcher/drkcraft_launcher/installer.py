666c91b79fea131d535edcd3ce2347dceea6e0a6
D3r3k23 <d3r3k523@gmail.com>
2022-04-23 07:22:49 -0700

    Fixed linux build action

--- launcher/drkcraft_launcher/installer.py
+++ launcher/drkcraft_launcher/installer.py
@@ -9,10 +9,5 @@ def run():
 def get_window_size() -> tuple[int, int]:
     return ( 600, 480 )
 
-def get_base_installation_dir() -> Optional[str]:
-    match PLATFORM:
-        case 'Window' : app_dir = os.path.expandvars('${LOCALAPPDATA}\\Programs')
-        case 'Linux'  : app_dir = os.path.expandvars('${HOME}')
-        case _: return None
-
-    return os.path.join(app_dir, 'DrkCraft')
+def get_base_installation_dir() -> str:
+    return os.path.join(os.path.expanduser('~'), 'drkcraft')
